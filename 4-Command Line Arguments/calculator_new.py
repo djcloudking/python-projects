1148dd6e8ac8eb64a47fc40c4c68a17d6307d0ae
djkone <djkone2025@gmail.com>
2024-06-11 23:10:10 -0500

    Update calculator_new.py

--- 4-Command Line Arguments/calculator_new.py
+++ 4-Command Line Arguments/calculator_new.py
@@ -12,4 +12,10 @@ def mul(a, b):
     mul = a * b
     return mul
 
-num1 = sys.arg[1]
+num1 = int(sys.argv[1])
+operation = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if operation == "add":
+    output = add(num1, num2)
+    print(output)
