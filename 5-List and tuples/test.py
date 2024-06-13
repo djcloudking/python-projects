1f2ab1db981bfcb15bcc8ca96b803f07a254e40a
djkone <djkone2025@gmail.com>
2024-06-13 11:11:06 -0500

    List and tuple test

--- 5-List and tuples/test.py
+++ 5-List and tuples/test.py
@@ -1,4 +1,8 @@
 # list
 
-student_names = [dj, hamza, malik, john]
-print(student_names)
+s3_bucket_list = ["dj_bucket", "hamzi_bucket", "malik_bucket"]
+print(len(s3_bucket_list))
+
+s3_bucket_list.append("new_s3_bucket")
+print(len(s3_bucket_list))
+
