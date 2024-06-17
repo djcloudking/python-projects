7106e84e62aca64ea9320256b73137815db9c1b1
djkone <djkone2025@gmail.com>
2024-06-17 16:39:59 -0500

    Provide EC2 instance info

---
+++ 8-dictionary and set/ec2_instance.py
@@ -0,0 +1,18 @@
+# Provide EC2 instance info
+
+ec2_instance = [
+    {
+        "id": "instance-01",
+        "type": "t2.micro"
+    },
+    {
+        "id": "instance-02",
+        "type": "t2.medium"
+    },
+    {
+        "id": "instance-03",
+        "type": "t2.xlarge"
+    }
+]
+
+print(ec2_instance[1]["type"])
