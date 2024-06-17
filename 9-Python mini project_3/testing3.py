438a6a6f15c3f2fa062003a9c8382f23e620bf14
djkone <djkone2025@gmail.com>
2024-06-17 17:56:55 -0500

    Update testing3.py

--- 9-Python mini project_3/testing3.py
+++ 9-Python mini project_3/testing3.py
@@ -1,1 +1,13 @@
 #Get pull request information on a repository using python
+#the repository we selected is kubernetes
+#Before writing the first piece of code, first install requests in your folder (pip install requests)
+
+#now import requests
+
+import requests
+
+requests.get("api.github.com/repos/kubernetes/kubernetes/pulls")
+
+
+
+
