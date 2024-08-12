a8ac6401aa0b63c1b56296b9ffbed6e975c60a74
djkone <djkone2025@gmail.com>
2024-08-12 12:39:03 -0500

    Create a list with the name and size of each file in a directory

--- 94_Python project/file_info.py
+++ 94_Python project/file_info.py
@@ -1,12 +1,13 @@
 import os
 
+# initialize an empty list to staore the dictionaries that contain the file info
 fileList = [] 
 
-for file in os.listdir():
-    if os.path.isfile(file):
+for file in os.listdir(): # Iterate through the list of files in the directory
+    if os.path.isfile(file): # checking to validate that file is a regular file
         fileInfo = {
             'File Name': file, 'size': os.path.getsize(file)
-        }
+        } # adding the file information to the dictionary
 
-        fileList.append(fileInfo)
+        fileList.append(fileInfo) # adding the file_Info dictionaries to the list
 print(fileList)
